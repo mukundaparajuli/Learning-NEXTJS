a57c95bb01443586c86d363fc0d8d6b37800e841
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-06-02 18:07:54 +0545

    not-found page and file colocation

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>404: Not Found</h1>
+      <div>Requested Page Could Not be Found!</div>
+    </>
+  );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 export default function ReviewDetails({
   params,
 }: {
@@ -6,6 +7,9 @@ export default function ReviewDetails({
     reviewId: string;
   };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for the product {params.productId}
