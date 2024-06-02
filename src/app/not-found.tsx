dd33b0a18410fca9614cef37ab5665a235751103
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-06-02 23:28:16 +0545

    private routes and route groups

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <>
       <h1>404: Not Found</h1>
-      <div>Requested Page Could Not be Found!</div>
+      <div>Could not find the requested page!</div>
     </>
   );
 }
